b25a955704b833d69bbc4febda65e64d8b86fc06
nilsmanc <etereaiberia@gmail.com>
2022-11-20 18:13:27 +0300

    create year

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import styles from './App.module.scss'
 import { createDate } from './utils/createDate'
 import { createMonth } from './utils/createMonth'
+import { createYear } from './utils/createYear'
 
 console.log('createDate', createDate({ locale: 'en-US' }))
 console.log('createMonth', createMonth({ locale: 'en-US' }).createMonthDays())
+console.log('createYear', createYear({ locale: 'en-US' }).createYearMonths())
 
 function App() {
   return <div className={styles.wrapper}>Calendar</div>
